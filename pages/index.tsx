2edc5748566235c9c98ef92b747080adeb7d92eb
mentorias152 <pghuy18@apcs.fitus.edu.vn>
2023-06-22 20:23:40 +0700

    continue coding

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Header/>
-        <Profile/>
-        <Blog/>
+        <Header />
+        <Profile />
+        <Blog />
       </main>
     </div>
     
