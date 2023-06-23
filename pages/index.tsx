de681f32b78d92911d39970aa4f16d3ae6f2367b
mentorias152 <pghuy18@apcs.fitus.edu.vn>
2023-06-23 20:42:46 +0700

    change logo font

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,25 +5,7 @@ import Header from '../components/header'
 import Profile from '../components/profile'
 import Blog from '../components/blog'
 
-import { useState } from 'react'
-
 export default function Home() {
-
-  const [burgerClass, setBurgerClass] = useState(styles.header_menu_bar + ' ' + styles.header_menu_unclicked)
-  const [isMenuClicked, setIsMenuClicked] = useState(true);
-  const [menuListClass, setMenuListClass] = useState(styles.header_menu_list + ' ' + styles.hidden)
-  
-  const handleClickMenu = () => {
-    if (isMenuClicked) {
-      setBurgerClass(styles.header_menu_bar + ' ' + styles.header_menu_clicked);
-      setMenuListClass(styles.header_menu_list + ' ' + styles.visible);
-    }
-    else {
-      setBurgerClass(styles.header_menu_bar + ' ' + styles.header_menu_unclicked);
-      setMenuListClass(styles.header_menu_list + ' ' + styles.hidden);
-    }
-    setIsMenuClicked(!isMenuClicked)
-  }
 
   return (
     <div className={styles.container}>
@@ -32,12 +14,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-
-        <div className={styles.header_menu} onClick={handleClickMenu}>
-          <div className={burgerClass}></div>
-          <div className={burgerClass}></div>
-          <div className={burgerClass}></div>
-        </div>
         <Header />
         <Profile />
         <Blog />
